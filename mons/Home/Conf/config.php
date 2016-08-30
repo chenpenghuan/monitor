967ed0ed051631a5038d5d2d626405142391c9ff
chenpenghuan <cphchenpenghuan@gmail.com>
2016-08-30 17:06:19 +0800

    修改应用目录名称为mons

---
+++ mons/Home/Conf/config.php
@@ -0,0 +1,22 @@
+<?php
+return array(
+	//'配置项'=>'配置值',
+	//'URL_INDEX' => 'http://localhost/monitor/home/index/index/#1',
+	//'DEFAULT_C_LAYER' => 'Search',
+	'URL_INDEX' => '/monitor/home/index/index',
+	//'URL_STATUS' => 'http://localhost/monitor/home/index/chkstatus',
+	'URL_STATUS' => '/monitor/home/index/chkstatus',
+	//'URL_LOGIN' => 'http://localhost/monitor/home/index/login',
+	'URL_LOGIN' => '/monitor/home/index/login',
+	//'URL_OUTIMG' => 'http://localhost/mysite/index.php/Home/Index/createimg',
+	'URL_PUBLIC' => '/monitor/Public',
+	'TMPL_L_DELIM' => '<{',
+	'TMPL_R_DELIM' => '}>',
+	'DB_TYPE' => 'mysql',
+	'DB_HOST' => 'localhost',
+	'DB_NAME' => 'winnerlook',
+	'DB_USER' => 'root',
+	'DB_PWD' => '123123',
+	'DB_PORT' => '3306',
+	'DB_PREFIX' => '', //设置表前缀
+);
---
+++ mons/Home/Controller/SearchController.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+
+class SearchController extends Controller {
+	public function hello() {
+		echo 'hello,thinkphp!';
+	}
+}
+?>
