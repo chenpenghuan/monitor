c47a63019134e300d921f15e91d1f895602a7542
chenpenghuan <cphchenpenghuan@gmail.com>
2016-10-12 15:09:36 +0800

    添加示例接口文件

---
+++ exam/test2.php
@@ -0,0 +1,12 @@
+<?php
+	$cont1[0]['test1']='属于二级测试1的内容';
+	$cont1[0]['test2']='测试22的内容';
+	$cont1[1]['test1']='属于二级测试2的内容（第二行）';
+	$cont1[1]['test2']='测试22的内容（第二行）';
+	echo json_encode($cont1);
+/*
+	$cont2['warn_cont']='通道1098出现拥堵，已阻断1398条数据';
+	$cont2['warn_true']='true';
+	echo json_encode($cont2);
+ */
+?>
